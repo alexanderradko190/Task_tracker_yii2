f956529cd92f825a4401a73b1d16f8d2492689f3
a.radko <a.radko@all-evak.ru>
2024-02-24 17:13:03 +0300

    fix

--- views/info/_tasks_tab.php
+++ views/info/_tasks_tab.php
@@ -3,8 +3,7 @@
 use yii\bootstrap5\Html;
 
 foreach ($tasks as $task) {
-    echo
-        Html::a($task->name, ['task/' . $task->id], ['class' => 'my-link text-decoration-none text-black', 'target' => '_blank']);
+    echo Html::a($task->name, ['task/' . $task->id], ['class' => 'my-link text-decoration-none text-black', 'target' => '_blank']);
     echo '<br>';
 }
 ?>
